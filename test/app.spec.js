2045928fb009fdd3408a2ab938654e6487f70d35
matth3wn <matth3wn@gmail.com>
2019-02-27 14:17:39 -0800

    finished testing

--- test/app.spec.js
+++ test/app.spec.js
@@ -20,33 +20,52 @@ describe('Testing bookmarks endpoints...', () => {
   })
   before(() => db(tableName).truncate());
 
-  afterEach(() => db(tableName).truncate());
+  
   after(() => db.destroy());
 
 
 
   describe('testing with data', () => {
-    // it('GET /bookmarks returns something', () => {
-    //   return supertest(app)
-    //     .get('/bookmarks')
-    //     .expect(200);
-    // });
+    it('GET /bookmarks returns something', () => {
+      return supertest(app)
+        .get('/bookmarks')
+        .expect(200);
+    });
     context('testing with data', () => {
       beforeEach(() => db.into(tableName).insert(bookmarks));
+      afterEach(() => db(tableName).truncate());
 
       it('DELETE /bookmarks should delete bookmark', () => {
-        let id =2;
+        let id = 2;
+        return supertest(app)
+          .delete(`/bookmarks/${id}`)
+          .expect(204);
+      })
+      
+      it('UPDATE /bookmarks should update bookmark', () => {
+        const title = { 
+          title: '2MDN',
+          url: '4https://developer.mozilla.org',
+          description: 'The4 only place to find web documentation',
+          rating: 4 }
 
-        db(tableName)
-          .select('*')
-          .then(results => {
-            console.log(results)
-            // id = results[0].id;
-            console.log(id);
-            return supertest(app)
-              .delete(`/bookmarks/${id}`)
-              .expect(204);
-          })
+          return supertest(app)
+            .patch('/bookmarks/3')
+            .send(title)
+            .expect(204);
+      })
+
+      it('INSERT /bookmark should insert a new bookmark', ()=>{
+        const newBookmark = { 
+          title: '2MDN',
+          url: '4https://developer.mozilla.org',
+          description: 'The4 only place to find web documentation',
+          rating: 4 }
+
+          return supertest(app)
+            .post('/bookmarks')
+            .send(newBookmark)
+            .expect(204);
       })
 
     })
